6785466ff5ca940515cf1746fa9c48752de9b609
Peter Teuben <teuben@gmail.com>
2018-07-05 13:46:16 -0400

    few simple counters

---
+++ tools/tutorial.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+
+from __future__ import print_function
+
+import xlrd
+ 
+def open_file(path):
+    book = xlrd.open_workbook(path)
+    ns = book.nsheets
+    s0 = book.sheet_by_index(0)
+    n = s0.nrows
+    for row in range(3,n):
+        fname = s0.cell(row,3).value
+        lname = s0.cell(row,4).value
+        tut = s0.cell(row,22).value
+        #print   '"',fname,lname,'" <',email,'>'
+        print(tut)
+        
+
+ 
+if __name__ == "__main__":
+    path = "ADASS 2018  Total Registrant Re.xls"
+    open_file(path)
