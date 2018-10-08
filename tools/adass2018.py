607d69f66ab636ea5eb978f1e24cde3ca675c186
Peter Teuben <teuben@gmail.com>
2018-10-08 14:18:36 -0400

    try out some html output

--- tools/adass2018.py
+++ tools/adass2018.py
@@ -8,12 +8,18 @@ from __future__ import print_function
 
 import xlrd
 import sys
+import io
 
 
 # names of the 3 sheets we got from C&VS (notice the 31 character limit of the basename)
 _p1 = 'ADASS 2018  Submitted Abstracts.xls'  
 _p2 = 'ADASS 2018  2nd Submitted Abstr.xls'
 _p3 = 'ADASS 2018  Total Registrant Re.xls'
+
+_header1 = '<html> <body>\n'
+_footer1 = '</body> </html>\n'
+
+
 
 class adass(object):
     def __init__(self, dirname, debug=False):
@@ -72,18 +78,36 @@ class adass(object):
             print("Accepted %d entries" % len(s))
         return (s,row_values)
 
-    def tab2list(self, filename):
-        """ filename with "names; Code"   - return the [names]
+    def tab2list(self, filename, use_code=False):
+        """ filename with "NAMES; Code"   - return the [names]
+            if <NAMES> is <FNAME LNAME>, it adds them as <LNAME, FNAME>
         """
         o1 = io.open(filename, encoding="utf-8").readlines()
+        o2 = []
+        o3 = []
+        nz = 0
         for i in range(len(o1)):
-            s = o1[i]
+            s = o1[i].strip() 
+            if s=='#': continue      # skip comment lines
+            if len(s) == 0: continue;   # skip blank lines
             isc = s.find(';')
             if isc < 0:
-                o1[i] = s.strip()
+                name = s.strip()
+                nz = nz + 1
+                code = 'Z%d' % nz
             else:
-                o1[i] = s[:isc].strip()
-        return o1
+                name = s[:isc].strip()
+                code = s[isc+1:].strip()
+            ic = name.find(',')
+            if ic < 0:
+                names = name.split()
+                if len(names) == 2:
+                    name = names[1] + ', ' + names[0]
+            o2.append(name)
+            o3.append(code)
+        if use_code:
+            return (o2,o3)
+        return o2
 
     def print_col(self, col):
         """ print a given columns. expert mode
@@ -103,6 +127,53 @@ class adass(object):
             print(key)
 
     def report_1(self, abstract=False, cat = False):
+        keys = list(self.x1.keys())
+        keys.sort()
+        for key in keys:
+            present   = self.x1[key][22].value
+            title1    = self.x1[key][23].value
+            abstract1 = self.x1[key][24].value
+            theme1    = self.x1[key][20].value
+            theme1a   = self.x1[key][21].value
+            r = self.x3[key]
+            focus_demo = r[25+self.off].value
+            demo_booth = r[26+self.off].value
+            email      = r[14+self.off].value
+            theme = theme1[:theme1.find(')')]
+                
+            if abstract: print(" ")
+            
+            if present == 'Talk/Focus Demo':
+                if focus_demo == '1' and demo_booth == '1':
+                    ptype = "F+B"
+                elif focus_demo == '1':
+                    ptype = "F"                    
+                elif demo_booth == '1':
+                    ptype = "B"                    
+                else:
+                    ptype = "O"                    
+            elif present == 'Poster':
+                ptype = "P"
+            else:
+                ptype = "X"
+                
+            if cat:
+                print("%s    ; %s%s." % (key,ptype,theme))
+            else:
+                print(ptype,key,email,title1)
+
+            if abstract:
+                print("    ABS:",abstract1)
+                print("    T:",theme1)
+                print("   TO:",theme1a)
+            if key in self.x2:
+                present2  = self.x2[key][22].value
+                title2    = self.x2[key][23].value
+                abstract2 = self.x2[key][23].value
+                print("  ABS2",key,title2)
+                if abstract: print("    ABS:",abstract2)
+
+    def report_1a(self, tabname = None):
         keys = list(self.x1.keys())
         keys.sort()
         for key in keys:
@@ -198,6 +269,54 @@ class adass(object):
                 else:
                     print(key,'-',title1)
 
+    def report_3a(self,o1,o2, count=False, dirname='www/abstracts'):
+        """ report a selection of presenters based on list of names
+        """
+        # prepare list of last names
+        lnames=[]
+        keys  =[]
+        for key in self.x1.keys():
+            lnames.append(key[:key.find(',')])
+            keys.append(key)
+        #print(lnames)
+        #print(keys)
+        #
+        n=0
+        for (k,c) in zip(o1,o2):
+            if k == '#': continue
+            found = False
+            if k in self.x1.keys():
+                # full match
+                found = True
+                key = k
+            else:
+                # try a partial match based on just last name
+                if lnames.count(k) == 1: 
+                    key = keys[lnames.index(k)]
+                    found = True                    
+                else:
+                    # one last try, min. match if 'k' is in lnames[]
+                    # for names in lnames:
+                    print("# %s" % k)
+            if found:
+                n         = n + 1
+                present   = self.x1[key][22].value
+                title1    = self.x1[key][23].value
+                abstract1 = self.x1[key][24].value
+                if count:
+                    print(n,key,present,title1)
+                else:
+                    fn = dirname + '/' + c + '.html'
+                    fp = open(fn,'w')
+                    fp.write(_header1)
+                    msg = '<b>%s: %s</b>\n' % (c, key)   ; fp.write(msg)
+                    msg = '<br>\n'                       ; fp.write(msg)                    
+                    msg = '<i>%s</i>\n' % title1         ; fp.write(msg)
+                    msg = '<br>\n'                       ; fp.write(msg)                                        
+                    msg = '%s\n' % abstract1             ; fp.write(msg)
+                    fp.write(_footer1)
+                    fp.close()
+                    
     def report_4(self, full = False, name=None):
         """ report emails only"""
 
