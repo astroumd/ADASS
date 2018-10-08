538c640d0aa1fff5fc73a12dcc16a4db89de8523
Peter Teuben <teuben@gmail.com>
2018-10-08 14:21:20 -0400

    using new report_3a

--- tools/abstracts.py
+++ tools/abstracts.py
@@ -11,4 +11,11 @@ if __name__ == "__main__":
     debug = True
     a = adass.adass('reg', debug)
     
-    a.report_1(True)
+    if len(sys.argv) == 2:
+        (o1,o2) = a.tab2list(sys.argv[1],True)
+        print(o1)
+        print(o2)
+        print(len(o1),len(o2))
+        a.report_3a(o1,o2)
+    else:
+        a.report_1(True)
