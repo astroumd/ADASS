7e1486a85b7af03df0bc98b77676f9f8c66c354b
Peter Teuben <teuben@gmail.com>
2018-10-22 14:45:53 -0400

    flexible absttract generation

--- tools/abstracts.py
+++ tools/abstracts.py
@@ -11,13 +11,14 @@ if __name__ == "__main__":
     debug = True
     a = adass.adass('reg', debug)
     
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
+        add_themes = int(sys.argv[2])
         (o1,o2,o3) = a.tab2list(sys.argv[1],True)
         print(o1)
         print(o2)
         print(o3)
         print(len(o1),len(o2),len(o3))
-        a.report_3a(o1,o2,o3)
+        a.report_3a(o1,o2,o3,themes=add_themes)
         a.report_3b(o1,o2,o3)        
     else:
         a.report_1(True)
