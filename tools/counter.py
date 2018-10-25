c3744a2de0a9af5384f9850505478155dc80ee1d
Peter Teuben <teuben@gmail.com>
2018-10-25 13:37:15 -0400

    verbose option

--- tools/counter.py
+++ tools/counter.py
@@ -10,7 +10,10 @@ import adass2018 as adass
 if __name__ == "__main__":
     a = adass.adass('reg')
     col = int(sys.argv[1])
+    verbose = False
+    if len(sys.argv) > 2:
+        verbose = True
     if col < 0:
         a.report_0()
     else:
-        a.print_col(col)
+        a.print_col(col,verbose)
